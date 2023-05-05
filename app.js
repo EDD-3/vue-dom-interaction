f82745c5ebd801b24718d91951d8b95370785d02
Ed <eduramq@gmail.com>
2023-05-04 19:33:57 -0700

    Vue data binding and vue app mounting

--- app.js
+++ app.js
@@ -2,9 +2,20 @@ const app = Vue.createApp({
     data () {
         //Always needs to return an object
         return {
-            courseGoal: 'Finish the course and learn Vue!'
+            courseGoalA: 'Finish the course and learn Vue!',
+            courseGoalB: 'Master Vue and build amazing apps!',
+            courseGoal: 'Finish the course and learn Vue!',
+            vueLink: 'https://vuejs.org/',
+        }
+    },
+    methods: {
+        outputGoal () {
+
+            const randomNumber = Math.random ;
+            return randomNumber < 0.5  ? this.courseGoalA: this.courseGoalB;
         }
     }
 });
 
+//Only one Vue app can be connected to one html element via a CSS selector
 app.mount("#user-goal");
